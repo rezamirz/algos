72929b5c3c00eae532219c8e0448d1d5296353cf
Reza Mir <rezamirz@gmail.com>
2020-04-05 12:36:00 -0400

    Add concurrent tests for file log

--- mylog/log_test.go
+++ mylog/log_test.go
@@ -48,6 +48,45 @@ func TestSimpleLogRotation(t *testing.T) {
 }
 
 func TestSimpleLogRotationMultithreaded(t *testing.T) {
+	cleanLogs()
+
+	configurator := configurator.NewConfigurator()
+	configurator.Put(LOGTYPE, "file")
+	configurator.Put(FILENAME, "mylog.log")
+	configurator.Put(LOGFILE_SIZE, "1000")
+	configurator.Put(LOG_ROTATION, "10")
+	log, err := New(configurator)
+	assert.NoError(t, err)
+
+	err = log.Open()
+	assert.NoError(t, err)
+	assert.Equal(t, 1, log.GetRotation())
+
+	logger := log.GetLogger("test1")
+
+	done := make(chan bool)
+	go func(logger Logger) {
+		logger.SetLevel(LevelInfo)
+		for i:=0; i<50; i++ {
+			logger.Info("Loop %d", i)
+		}
+		done <- true
+	} (logger)
+
+	go func(logger Logger) {
+		logger.SetLevel(LevelInfo)
+		for i:=50; i<100; i++ {
+			logger.Info("Loop %d", i)
+		}
+		done <- true
+	} (logger)
+
+	<- done
+	<- done
+
+	assert.Equal(t, 5, log.GetRotation())
+
+	log.Close()
 }
 
 func TestFullLogRotation(t *testing.T) {
@@ -96,4 +135,41 @@ func TestFullLogRotation(t *testing.T) {
 }
 
 func TestFullLogRotationMultithreaded(t *testing.T) {
+	cleanLogs()
+
+	configurator := configurator.NewConfigurator()
+	configurator.Put(LOGTYPE, "file")
+	configurator.Put(FILENAME, "mylog.log")
+	configurator.Put(LOGFILE_SIZE, "1000")
+	configurator.Put(LOG_ROTATION, "10")
+	log, err := New(configurator)
+	assert.NoError(t, err)
+
+	err = log.Open()
+	assert.NoError(t, err)
+	assert.Equal(t, 1, log.GetRotation())
+
+	logger := log.GetLogger("test1")
+	logger.SetLevel(LevelInfo)
+
+	done := make(chan bool)
+	go func(logger Logger) {
+		for i := 0; i < 100; i++ {
+			logger.Info("Loop %d", i)
+		}
+		done <- true
+	} (logger)
+
+	go func(logger Logger) {
+		for i := 100; i < 200; i++ {
+			logger.Info("Loop %d", i)
+		}
+		done <- true
+	} (logger)
+
+	<- done
+	<- done
+
+	assert.Equal(t, 10, log.GetRotation())
+	log.Close()
 }
