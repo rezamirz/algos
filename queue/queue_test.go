128f225fb3edd882e552268025387d06273e8de4
Reza Mir <rezamirz@gmail.com>
2022-02-06 00:32:46 -0500

    Add queue

---
+++ queue/queue_test.go
@@ -0,0 +1,45 @@
+package queue
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFifo(t *testing.T) {
+	q := New()
+
+	q.Push(1)
+	q.Push(2)
+	q.Push(3)
+	assert.Equal(t, 3, q.Len())
+
+	v := q.Pop()
+	assert.Equal(t, 1, v)
+	v = q.Pop()
+	assert.Equal(t, 2, v)
+	v = q.Pop()
+	assert.Equal(t, 3, v)
+	assert.Equal(t, 0, q.Len())
+
+	for i:=1; i<=100; i++ {
+		q.Push(i)
+	}
+	assert.Equal(t, 100, q.Len())
+
+	for i:=1; i<=100; i++ {
+		v = q.Pop()
+		assert.Equal(t, i, v)
+	}
+	assert.Equal(t, 0, q.Len())
+}
+
+func TestFifo2(t *testing.T) {
+	q := New()
+
+	q.Push(1)
+	assert.Equal(t, 1, q.Len())
+
+	v := q.Pop()
+	assert.Equal(t, 1, v)
+	assert.Equal(t, 0, q.Len())
+}
